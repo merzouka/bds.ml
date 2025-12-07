516c1f9b665edd5e58756389f50b778b273e7a64
merzouka <marzoukayouness@gmail.com>
2025-12-07 20:50:18 +0100

    Added deploy logic

---
+++ deploy/streamlit_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("BotNet Intrusion Detection System")
+st.write(
+    "This is a smart IDS for detecting the type of BotNet attack using properties of the network traffic"
+)
