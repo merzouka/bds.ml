5c240a6d185229de1e3e4023636c6266609b1e5e
merzouka <marzoukayouness@gmail.com>
2025-12-08 22:20:57 +0100

    Random change

--- deploy/tmp.py
+++ deploy/tmp.py
@@ -1,4 +1,6 @@
 import joblib
 
-model = joblib.load('./models/no-pre/kmeans_k2_model.joblib')
-print(model.cluster_centers_)
+model = joblib.load('./models/no-pre/gmm_model.pkl')
+print(model.keys())
+scaler = model['scaler']
+joblib.dump(scaler, './models/no-pre/gmm_scaler.joblib')
